90f870e623d2507bc2f4e45542c12fa2c2f8a39f
Aaron Hosford <hosford42@gmail.com>
2022-01-03 12:10:36 -0600

    Cleaned up some bugs in input attention

--- cephalus/modules/input_attention.py
+++ cephalus/modules/input_attention.py
@@ -25,24 +25,27 @@ class StandardInputAttentionProvider(InputAttentionProvider):
 
     _query_model: Model = None
     _key_model: Model = None
+    _value_model: Model = None
     _attention_layer: Attention = None
 
     def configure(self, kernel: 'StateKernel') -> None:
         super().configure(kernel)
         self._query_model = Sequential(
-            clone_model(kernel.config.model_template).layers[:-1],
-            Dense(self.input_width)
-        )
-        self._key_model = Sequential(
             clone_model(kernel.config.model_template).layers[1:-1],
             Dense(self.input_width)
         )
+        self._key_model = Sequential(
+            clone_model(kernel.config.model_template).layers[:-1],
+            Dense(self.input_width)
+        )
+        self._value_model = clone_model(self._key_model)
         self._attention_layer = Attention()
 
     def get_trainable_weights(self) -> List[tf.Variable]:
         return self._key_model.trainable_weights
 
-    def get_loss(self, previous_frame: StateFrame) -> Optional[tf.Tensor]:
+    def get_loss(self, previous_frame: 'StateFrame',
+                 current_frame: 'StateFrame') -> Optional[tf.Tensor]:
         return None  # No intrinsic loss.
 
     def attend_inputs(self, frame: 'StateFrame') -> None:
@@ -51,23 +54,22 @@ class StandardInputAttentionProvider(InputAttentionProvider):
         assert frame.previous_state is not None
         assert frame.attended_input_tensor is None
 
+        query = self._query_model(frame.previous_state[tf.newaxis, :])[tf.newaxis, :, :]
+
         input_tensors = tf.concat([input_tensor[tf.newaxis, :]
                                    for input_tensor in frame.input_tensors],
                                   axis=0)
         previous_state = tf.repeat(frame.previous_state[tf.newaxis, :], input_tensors.shape[0],
                                    axis=0)
-
-        # query.shape == (batch_size, query_count, channels)
-        query = self._query_model(frame.previous_state[tf.newaxis, :])[tf.newaxis, :, :]
-        assert query.shape == (1, 1, self.input_width)
-
-        # values.shape == (batch_size, value_count,        channels)
-        values = input_tensors[tf.newaxis, :, :]
-        assert values.shape == (1, len(input_tensors), self.input_width)
-
-        # keys.shape == (batch_size, value_count,        channels)
         keys = self._key_model([previous_state, input_tensors])
+        values = self._value_model([previous_state, input_tensors])
+
+        # query.shape: (batch_size, query_count, channels)
+        # keys.shape: (batch_size, value_count, channels)
+        # values.shape: (batch_size, value_count, channels)
+        assert query.shape == (1, 1, self.input_width)
         assert keys.shape == (1, len(input_tensors), self.input_width)
+        assert values.shape == (1, len(input_tensors), self.input_width)
 
         # attended_values.shape == (batch_size, query_count, channels)
         attended_values = self._attention_layer([query, values, keys])
